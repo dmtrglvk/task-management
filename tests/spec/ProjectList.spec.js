543303bbf7d07131f9b0ecc998100ff5b6fec8f5
Dmytro Golovko <dmytro.golovko@knowit.dk>
2025-03-31 18:29:21 +0200

    add vitest to test components

---
+++ tests/spec/ProjectList.spec.js
@@ -0,0 +1,39 @@
+import { mount } from '@vue/test-utils'
+import { expect, test } from 'vitest'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { createStore } from 'vuex'
+import ProjectsList from '@/views/ProjectsList.vue'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+global.ResizeObserver = require('resize-observer-polyfill')
+
+const store = createStore({
+    modules: {
+        projects: {
+            namespaced: true,
+            state: {
+                projects: [{ id: 1, name: 'Test Project' }],
+                loading: false,
+            },
+            getters: {
+                projectsCount: (state) => state.projects.length,
+            }
+        }
+    }
+})
+
+test('displays projects count from store', () => {
+    const wrapper = mount(ProjectsList, {
+        global: {
+            plugins: [vuetify, store],
+        }
+    })
+    
+    expect(wrapper.text()).toContain('1')
+})
